3f19ad5d4d580f620e2ebc375596633b745d2440
lee <1825103317>
2021-03-09 21:12:35 +0800

    lee2

--- routes/users.js
+++ routes/users.js
@@ -6,9 +6,7 @@ var URL = require('url');
 
 router.get('/check',function(req,res,next){
   var result = ''
-  // http://localhost:8080/apis/users/getUserInfo?id=1
   var params = URL.parse(req.url, true).query;
-  // console.log(params);
   console.log(params.xh)
   console.log(params.pwd)
   helper.sql('select * from S where xh='+params.xh+"and pwd="+params.pwd, function (err, result) {
@@ -17,17 +15,30 @@ router.get('/check',function(req,res,next){
     }
     console.log(result)
     this.result = result
-    res.send(result)
+    if(result.recordset.length == 1){
+      res.send(result)
+    }
+    else{
+      helper.sql('select * from T where gh='+params.xh+"and pwd="+params.pwd, function (err, result) {
+        if (err) {
+          console.log(err);
+        }
+        console.log(result)
+        this.result = result
+        res.send(result)
+      })
+    }
+
   });
 })
 
-router.get('/getStudent',function(req,res,next){
+router.get('/getStudentChosen',function(req,res,next){
   var result = ''
   // http://localhost:8080/apis/users/getUserInfo?id=1
   var params = URL.parse(req.url, true).query;
   // console.log(params);
   console.log(params.xh)
-  helper.sql('select * from E where xh='+params.xh, function (err, result) {
+  helper.sql('select E.pid,P.pmc,E.gh,T.xm from E,T,P where E.gh = T.gh and E.pid = P.pid and E.xh = '+ params.xh, function (err, result) {
     if (err) {
       console.log(err);
     }
@@ -37,25 +48,90 @@ router.get('/getStudent',function(req,res,next){
   });
 })
 
-router.get('/add',function(req,res,next){
+router.get('/getTeacherChosen',function(req,res,next){
+  var result = ''
+  // http://localhost:8080/apis/users/getUserInfo?id=1
+  var params = URL.parse(req.url, true).query;
+  // console.log(params);
+  console.log(params.gh)
+  helper.sql('select F.pid,P.pmc,S.xh,S.xm from F,P,S where F.xh = S.xh and F.pid = P.pid and F.gh = '+ params.gh, function (err, result) {
+    if (err) {
+      console.log(err);
+    }
+    console.log(result)
+    this.result = result
+    res.send(result)
+  });
+})
+
+router.get('/getAllProjectsExceptSelected',function(req,res,next){
+  var result = ''
+  // http://localhost:8080/apis/users/getUserInfo?id=1
+  var params = URL.parse(req.url, true).query;
+  // console.log(params);
+  // console.log(params.xh)
+  helper.sql('select distinct* from O where O.pid not in(select A.pid from(select pid,gh from E where xh='+ params.xh+')as A)', function (err, result) {
+    if (err) {
+      console.log(err);
+    }
+    console.log(result)
+    this.result = result
+    res.send(result)
+  });
+})
+
+router.get('/getAllStudentsExceptSelected',function(req,res,next){
+  var result = ''
+  // http://localhost:8080/apis/users/getUserInfo?id=1
+  var params = URL.parse(req.url, true).query;
+  // console.log(params);
+  // console.log(params.xh)
+  helper.sql('select A.pid,P.pmc,A.xh,S.xm from ((select * from E where E.gh='+params.gh+') except (select * from F where F.gh='+params.gh+'))as A join S on S.xh=A.xh join P on P.pid=A.pid', function (err, result) {
+    if (err) {
+      console.log(err);
+    }
+    console.log(result)
+    this.result = result
+    res.send(result)
+  });
+})
+// insert into E values('005','8765001','18145012')
+router.get('/chooseProject',function(req,res,next){
   var result = ''
   // http://localhost:8080/apis/users/add?pid=001&pmc=个人事务管理系统&gh=8765001&xm=吴琪
   var params = URL.parse(req.url, true).query;
   console.log(params.pid);
-  console.log(params.pmc);
   console.log(params.gh);
-  console.log(params.xm);
   console.log(params.xh);
-  var insertstring = "'"+params.pid+"','"+params.pmc+"','"+params.gh+"','"+params.xm+"'"
+  var insertstring = "'"+params.pid+"','"+params.gh+"','"+params.xh+"'"
+
   // insert into O values('001','个人事务管理系统','8765001','吴琪')
   console.log(insertstring)
-  helper.sql("insert into O values("+insertstring+")", err => {
+  helper.sql("insert into E values("+insertstring+")", err => {
+    // insert into E values('001','个人事务管理系统','8765001','吴琪',  '18145001')
     if (err) {
       console.log("error:" + err);
-      res.send(0)
     } else {
       console.log("Ok!");
-      res.send(1)
+    }
+  });
+})
+
+router.get('/chooseStudent',function(req,res,next){
+  var result = ''
+  // http://localhost:8080/apis/users/add?pid=001&pmc=个人事务管理系统&gh=8765001&xm=吴琪
+  var params = URL.parse(req.url, true).query;
+  console.log(params.pid);
+  console.log(params.gh);
+  console.log(params.xh);
+  var insertstring = "'"+params.pid+"','"+params.gh+"','"+params.xh+"'"
+
+  console.log(insertstring)
+  helper.sql("insert into F values("+insertstring+")", err => {
+    if (err) {
+      console.log("error:" + err);
+    } else {
+      console.log("Ok!");
     }
   });
 })
@@ -65,7 +141,8 @@ router.get('/deletechosenproject',function(req,res,next){
   var params = URL.parse(req.url, true).query;
   console.log(params.pid);
   console.log(params.xh);
-  helper.sql("delete O where "+params.xh+"= xh and "+params.pid+" = pid", err => {
+  console.log(params.gh);
+  helper.sql("delete E where "+params.xh+"= xh and "+params.pid+" = pid and "+params.gh+"= gh", err => {
     if (err) {
       console.log("error:" + err);
     } else {
@@ -79,7 +156,8 @@ router.get('/deletechosenstudent',function(req,res,next){
   var params = URL.parse(req.url, true).query;
   console.log(params.pid);
   console.log(params.xh);
-  helper.sql("delete O where "+params.xh+"= xh and "+params.pid+" = pid", err => {
+  console.log(params.gh);
+  helper.sql("delete F where "+params.xh+"= xh and "+params.pid+" = pid and "+params.gh+" = gh", err => {
     if (err) {
       console.log("error:" + err);
     } else {
